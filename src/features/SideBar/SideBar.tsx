ab5790f5b0f04abbad68fc3d74aced00c38eb2fe
sobhan <sobhanjafarii87@gmail.com>
2026-08-26 14:12:08 +0330

    [ADD] side bar ui

--- src/features/SideBar/SideBar.tsx
+++ src/features/SideBar/SideBar.tsx
@@ -1,4 +1,5 @@
 import { ReactNode } from "react";
+import History from "./History";
 
 export default function SideBar(): ReactNode {
   return (
@@ -6,6 +7,8 @@ export default function SideBar(): ReactNode {
       <button className="p-4 w-full border border-gray-400 dark:border-gray-500 text-cyan-700 dark:text-cyan-500 text-xl rounded-2xl cursor-pointer transition-all duration-150 ease-in-out hover:-translate-y-1 active:opacity-30">
         گفتگو جدید +
       </button>
+
+      <History />
     </aside>
   );
 }
